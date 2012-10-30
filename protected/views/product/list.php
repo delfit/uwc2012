eb57959381c04692c323b400a41f403c616d2533
skynet <skynet@irudiy>
2012-10-31 02:05:51 +0300

    добавленно поиск по товарам

--- protected/views/product/list.php
+++ protected/views/product/list.php
@@ -11,10 +11,10 @@ $this->widget( 'zii.widgets.CListView', array(
 	'dataProvider' => $products,
 	'itemView' => '_product', 
 	'enablePagination' => true, 
-	'ajaxUpdate'=>false
-//	'pager' => array(
-//		'class' => 'CLinkPager',
-//		'pageSize' => 1
-//	)
+	'ajaxUpdate'=>false,
+	'pager' => array(
+		'class' => 'CLinkPager',
+		'pageSize' => 1
+	)
 ));
 ?>
