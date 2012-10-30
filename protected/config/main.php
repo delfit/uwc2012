027d98d41afc1ec9ac1985e7774fa73ae8e5bef6
xt99 <dermeister.online@gmail.com>
2012-10-30 12:23:04 +0200

    добавлена первоначальная разметка главной страницы

--- protected/config/main.php
+++ protected/config/main.php
@@ -6,7 +6,7 @@
 return array(
 	'basePath' => dirname( __FILE__ ) . DIRECTORY_SEPARATOR . '..',
 	
-	'name' => 'UWC 2012',
+	'name' => 'UWC2012',
 	
 	// preloading 'log' component
 	'preload' => array( 
@@ -101,5 +101,8 @@ return array(
 	'params' => array(
 		// this is used in contact page
 		'adminEmail' => 'webmaster@example.com',
+		
+		// количество колонок на главной странице
+		'mainPageColumnsCount' => 3
 	),
 );
