c6fe1d462d2f9784b93514a80c57004aa4cb8786
xt99 <dermeister.online@gmail.com>
2012-10-31 12:23:30 +0200

    подправлена конфигурация запуска тестов

--- protected/tests/travis/app-setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-sudo chmod -R 0777 ./assets
-sudo chmod -R 0777 ./protected/runtime
--- protected/tests/travis/selenium-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-wget -O /tmp/selenium-server-standalone.jar http://selenium.googlecode.com/files/selenium-server-standalone-2.25.0.jar
-java -jar /tmp/selenium-server-standalone.jar &
-
-export DISPLAY=:99.0
-sudo sh -e /etc/init.d/xvfb start
-sleep 3
