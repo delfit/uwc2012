77988cc0eb2f584607a3c1eebd851a7525cb4535
xt99 <dermeister.online@gmail.com>
2012-10-31 12:09:07 +0200

    подправлена конфигурация запуска тестов

--- protected/tests/functional/SiteTest.php
+++ protected/tests/functional/SiteTest.php
@@ -5,7 +5,9 @@ class SiteTest extends WebTestCase
 
 	public function testIndex() {
 		$this->open( '' );
+		echo '[SiteTest:after open]';
 		$this->assertTextPresent( 'UWC2012' );
+		echo '[SiteTest:after assertTextPresent]';
 	}
 
 }
