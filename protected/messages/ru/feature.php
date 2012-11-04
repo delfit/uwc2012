bb22db8b6d9dcf6901100430b412f84eb32dcd9b
xt99 <dermeister.online@gmail.com>
2012-11-04 12:44:10 +0200

    слито

--- protected/messages/ru/feature.php
+++ protected/messages/ru/feature.php
@@ -4,7 +4,10 @@ return array(
 	'Description' => 'Описание',
 	'Feature' => 'Характеристика',
 	'Features' => 'Характеристики',
-	'Feature used in products and can not be delated' => 'Характеристика используется в товарах и не может быть удалена',
+	'Feature ":featureName" created' => 'Характеристика ":featureName" создана',
+	'Feature ":featureName" was deleted' => 'Характеристика ":featureName" удалена',
+	'Feature ":featureName" was not deleted' => 'Характеристика ":featureName" не удалена',
+	'Feature ":featureName" used in products and can not be delated' => 'Характеристика ":featureName" используется в товарах и не может быть удалена',
 	'Name' => 'Название',
 	'New Feature' => 'Новая характеристика'
 );
--- protected/messages/uk/feature.php
+++ protected/messages/uk/feature.php
@@ -4,7 +4,10 @@ return array(
 	'Description' => 'Опис',
 	'Feature' => 'Характеристика',
 	'Features' => 'Характеристики',
-	'Feature used in products and can not be delated' => 'Характеристика використовується і не може бути видаленою',
+	'Feature ":featureName" created' => 'Характеристику ":featureName" створено',
+	'Feature ":featureName" was deleted' => 'Характеристику ":featureName" видалено',
+	'Feature ":featureName" was not deleted' => 'Характеристику ":featureName" не видалено',
+	'Feature ":featureName" used in products and can not be delated' => 'Характеристика ":featureName" використовується і не може бути видаленою',
 	'Name' => 'Назва',
 	'New Feature' => 'Нова характеристика'
 );
